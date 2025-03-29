583594c6af835573c467d63af6a33d43fa8435d3
tkl <tkl7665@gmail.com>
2025-03-29 20:01:42 +0800

    updated to include easyocr

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -1,3 +1,3 @@
 from .app import Captcha
 
-__version__='1.0.10'
+__version__='1.0.11'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
 	name='Captcha',
-	version='1.0.10',
+	version='1.0.11',
 	packages=find_packages(),
 	include_package_data=True,
 	package_data={
@@ -16,6 +16,7 @@ setup(
 		'albumentations',
 		'annotated-types',
 		'concurrent-log-handler',
+		'easyocr',
 		'filelock',
 		'fsspec',
 		'Jinja2',
